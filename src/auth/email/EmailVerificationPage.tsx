c6582e7d7958176ef7c49c46108a363829a0c94f
Anurag Deshmukh <deshmukh.anurag.611@gmail.com>
2026-04-28 19:52:54 +0000

    fix: use wasp/client/router for auth links

--- src/auth/email/EmailVerificationPage.tsx
+++ src/auth/email/EmailVerificationPage.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "wasp/client/router";
 import { VerifyEmailForm } from "wasp/client/auth";
 import { AuthLayout } from "../AuthLayout";
 
--- src/auth/email/PasswordResetPage.tsx
+++ src/auth/email/PasswordResetPage.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "wasp/client/router";
 import { ResetPasswordForm } from "wasp/client/auth";
 import { AuthLayout } from "../AuthLayout";
 
